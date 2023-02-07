f28df1720625e3afbcc1623927283512f03df9c6
jack9603301 <jack9603301@163.com>
2023-02-07 14:26:13 +0800

    Modify the main route table format to make it easier to install and maintain

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_ips(ASN_set, type):
     return result
 
 def main():
-    ips = get_ips([15169,36459,54113,32934,3356,62041,4826,16276,22611,19679,34282,16509,8075,14061,58182,64050,38197,206281,34240,13414,13335,24940,24572,23816,20446],"ipv4")
+    ips = get_ips([15169,36459,54113,32934,3356,62041,4826,16276,22611,19679,34282,16509,8075,14061,58182,64050,38197,206281,34240,13414,13335,24940,24572,23816,20446,14618],"ipv4")
     values =  json.dumps(ips, sort_keys=True, indent=4, separators=(',', ': '))
     #print(values)
     
@@ -31,7 +31,7 @@ def main():
         ASN = key
         for dict in value:
             prefix = dict["prefix"]
-            print(f"ip route {prefix} enp1s0")
+            print(f"ip route add {prefix} nexthop via 192.168.20.2 dev tun0 weight 1 nexthop via 192.168.40.2 dev tun1 weight 1")
 
 if __name__ == "__main__":
     main()
