88fae413735a92593fcd966c81be3ace7969fcd1
jack9603301 <jack9603301@163.com>
2023-02-09 15:50:02 +0800

    Add a one-way detection script to replace BFD, which must be used in conjunction with kernel parameters -- it may be necessary to enable the Ignore EMCP is not equivalent to the next hop option

---
+++ vyos_check_icmp_overlay_bfd.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from __future__  import print_function
+import subprocess
+import threading
+
+def is_reachable(ip):
+    if subprocess.call(["ping", "-W", "1", "-c", "1", ip])==0:#只发送两个ECHO_REQUEST包
+        return True
+    else:
+        return False
+
+def loop_main():
+    if(is_reachable("192.168.10.1") == False):
+        subprocess.call(["ip", "link", "set", "dev", "tun0", "down"])
+    else:
+        subprocess.call(["ip", "link", "set", "dev", "tun0", "up"])
+    if(is_reachable("192.168.30.1") == False):
+        subprocess.call(["ip", "link", "set", "dev", "tun1", "down"])
+    else:
+        subprocess.call(["ip", "link", "set", "dev", "tun1", "up"])
+
+if __name__ == "__main__":
+    loop_main()
